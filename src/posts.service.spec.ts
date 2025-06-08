ecbd219b486053cb76953d9f7ef1c63b3b9b9689
deniol54 <daniil.paholkov@yandex.ru>
2025-06-08 11:35:43 +0300

    Реализованы юнит тесты

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -14,9 +14,12 @@ describe('PostsService', () => {
 
   it('should add a new post', () => {
     // реализуйте тест-кейс
+    postsService.create(post);
+    expect(postsService.find('2')?.text).toEqual(post.text);
   });
 
   it('should find a post', () => {
     // реализуйте тест-кейс
+    expect(postsService.find('1')?.text).toEqual('Some pre-existing post');
   });
 });
